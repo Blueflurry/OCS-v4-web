6f544058c2297143ca3a4a97f3ca4cc919a4934f
siddhantj929 <siddhantj929@gmail.com>
2025-02-14 14:44:49 +0530

    added stay details page

---
+++ app/(pages)/stays/[stayId]/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./StayDetails.module.scss";
+
+const StayDetails = () => {
+    return (
+        <div className={styles["stay-details"]}>
+            <div className={styles["stay-details__carousel"]}></div>
+        </div>
+    );
+};
+
+export default StayDetails;
