50b9d25f88ccb6897cf6a32718049c80802fe70d
mskn06 <muskaan24mj@gmail.com>
2025-02-24 15:04:26 +0530

    ratings module

--- app/(pages)/stays/[stayId]/page.js
+++ app/(pages)/stays/[stayId]/page.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./StayDetails.module.scss";
-import StayCard from "@/app/components/StayCard";
 import ImageCarousel from "@/app/components/ImageCarousel";
 import { VILLA_IMAGES } from "@/app/data/dummy";
 import Categories from "@/app/components/Categories";
@@ -10,157 +9,183 @@ import Button from "@/app/components/Button";
 import Amenities from "@/app/components/Amenities";
 import ReturnPolicy from "@/app/components/ReturnPolicy";
 import Reviews from "@/app/components/Reviews";
+import Header from "@/app/modules/Header";
 
 const StayDetails = () => {
     return (
-        <div className={styles["stay-details"]}>
-            <div className={styles["stay-details__carousel"]}>
-                <div style={{ padding: "30px 0px 200px" }}>
-                    <ImageCarousel images={VILLA_IMAGES} />
-                    <Categories images={VILLA_IMAGES}></Categories>
+        <>
+            <Header></Header>
+            <div className={`${styles["stay-details"]} container`}>
+                <div className={styles["stay-details__carousel"]}>
+                    <div style={{ padding: "30px 0px 200px" }}>
+                        <ImageCarousel images={VILLA_IMAGES} />
+                        <Categories images={VILLA_IMAGES}></Categories>
 
-                    {/* progress bar */}
-                    <div className={styles["stay-details__progress-bar"]}>
-                        <span
-                            className={styles["stay-details__indicator-1"]}
-                        ></span>
-                        <span
-                            className={styles["stay-details__indicator-2"]}
-                        ></span>
-                    </div>
-
-                    {/* stay information */}
-                    <div className={styles["stay-details__content"]}>
-                        <div className={styles["stay-details__header"]}>
-                            <div className={styles["stay-details__rating"]}>
-                                <b>5.0</b>
-                                <Image
-                                    src="/assets/images/ratings.svg"
-                                    width={20}
-                                    height={20}
-                                    alt="Location"
-                                />
-                                <p>(300 Reviews)</p>
-                            </div>
+                        {/* progress bar */}
+                        <div className={styles["stay-details__progress-bar"]}>
+                            <span
+                                className={styles["stay-details__indicator-1"]}
+                            ></span>
+                            <span
+                                className={styles["stay-details__indicator-2"]}
+                            ></span>
                         </div>
-                        <div className={styles["stay-details__title"]}>
-                            <h3>Sereno By The Sea</h3>
-                            {/* <span className={styles["stay-details__separator"]} />
+
+                        {/* stay information */}
+                        <div className={styles["stay-details__content"]}>
+                            <div className={styles["stay-details__header"]}>
+                                <div className={styles["stay-details__rating"]}>
+                                    <b>5.0</b>
+                                    <Image
+                                        src="/assets/images/ratings.svg"
+                                        width={20}
+                                        height={20}
+                                        alt="Location"
+                                    />
+                                    <p>(300 Reviews)</p>
+                                </div>
+                            </div>
+                            <div className={styles["stay-details__title"]}>
+                                <h3>Sereno By The Sea</h3>
+                                {/* <span className={styles["stay-details__separator"]} />
                                 <h3>
                                     4 <span>BHK</span>
                                 </h3> */}
-                        </div>
-                        <p className={styles["stay-details__capacity"]}>
-                            <span
-                                className={
-                                    styles["stay-details__capacity--amount"]
-                                }
-                            >
-                                2
-                            </span>{" "}
-                            BHK
-                            <span
-                                className={styles["stay-details__separator"]}
-                            />
-                            <span
-                                className={
-                                    styles["stay-details__capacity--amount"]
-                                }
-                            >
-                                4
-                            </span>{" "}
-                            Guests
-                            <span
-                                className={styles["stay-details__separator"]}
-                            />
-                            <span className={styles["stay-details__partner"]}>
-                                Hosted by <span>Elivaas</span>
-                            </span>
-                        </p>
-                        <div className={styles["stay-details__price"]}>
-                            <div>
+                            </div>
+                            <p className={styles["stay-details__capacity"]}>
                                 <span
                                     className={
-                                        styles["stay-details__price--striked"]
+                                        styles["stay-details__capacity--amount"]
                                     }
                                 >
-                                    ₹65,500
-                                </span>
-                                <p
+                                    2
+                                </span>{" "}
+                                BHK
+                                <span
                                     className={
-                                        styles["stay-details__price--active"]
+                                        styles["stay-details__separator"]
+                                    }
+                                />
+                                <span
+                                    className={
+                                        styles["stay-details__capacity--amount"]
                                     }
                                 >
-                                    ₹45,500{" "}
+                                    4
+                                </span>{" "}
+                                Guests
+                                <span
+                                    className={
+                                        styles["stay-details__separator"]
+                                    }
+                                />
+                                <span
+                                    className={styles["stay-details__partner"]}
+                                >
+                                    Hosted by <span>Elivaas</span>
+                                </span>
+                            </p>
+                            <div className={styles["stay-details__price"]}>
+                                <div>
                                     <span
                                         className={
-                                            styles["stay-details__price--unit"]
+                                            styles[
+                                                "stay-details__price--striked"
+                                            ]
                                         }
                                     >
-                                        per night
+                                        ₹65,500
                                     </span>
+                                    <p
+                                        className={
+                                            styles[
+                                                "stay-details__price--active"
+                                            ]
+                                        }
+                                    >
+                                        ₹45,500{" "}
+                                        <span
+                                            className={
+                                                styles[
+                                                    "stay-details__price--unit"
+                                                ]
+                                            }
+                                        >
+                                            per night
+                                        </span>
+                                    </p>
+                                </div>
+                                <p
+                                    className={
+                                        styles["stay-details__price--person"]
+                                    }
+                                >
+                                    Equals to <b>₹10,000</b> per person
                                 </p>
                             </div>
-                            <p
+
+                            {/* date picker */}
+                            <div
+                                className={styles["stay-details__date-picker"]}
+                            >
+                                <DateRangePicker></DateRangePicker>
+                            </div>
+
+                            <div
                                 className={
-                                    styles["stay-details__price--person"]
+                                    styles["stay-details__location_group"]
                                 }
                             >
-                                Equals to <b>₹10,000</b> per person
-                            </p>
-                        </div>
-
-                        {/* date picker */}
-                        <div className={styles["stay-details__date-picker"]}>
-                            <DateRangePicker></DateRangePicker>
-                        </div>
-
-                        <div className={styles["stay-details__location_group"]}>
-                            <div className={styles["stay-details__location"]}>
-                                <Image
-                                    src="/assets/images/location.svg"
-                                    width={20}
-                                    height={20}
-                                    alt="Location"
-                                />
-                                <p>Asagao, Goa</p>
-                            </div>
-                            <div
-                                className={styles["stay-details__locationBtn"]}
-                            >
-                                <Button>
+                                <div
+                                    className={styles["stay-details__location"]}
+                                >
                                     <Image
-                                        src="/assets/images/redirect.svg"
-                                        alt="Search"
+                                        src="/assets/images/location.svg"
                                         width={20}
                                         height={20}
+                                        alt="Location"
                                     />
-                                    Open in map
-                                </Button>
+                                    <p>Asagao, Goa</p>
+                                </div>
+                                <div
+                                    className={
+                                        styles["stay-details__locationBtn"]
+                                    }
+                                >
+                                    <Button>
+                                        <Image
+                                            src="/assets/images/redirect.svg"
+                                            alt="Search"
+                                            width={20}
+                                            height={20}
+                                        />
+                                        Open in map
+                                    </Button>
+                                </div>
                             </div>
                         </div>
+                        {/* map */}
+                        <div className={styles["stay-details__map"]}>
+                            <Image
+                                src="/assets/images/map.svg"
+                                width={400}
+                                height={250}
+                                alt="Map"
+                            />
+                        </div>
+
+                        {/* amenities */}
+                        <Amenities></Amenities>
+
+                        {/* policy */}
+                        <ReturnPolicy></ReturnPolicy>
+
+                        {/* reviews & ratings */}
+                        <Reviews></Reviews>
                     </div>
-                    {/* map */}
-                    <div className={styles["stay-details__map"]}>
-                        <Image
-                            src="/assets/images/map.svg"
-                            width={400}
-                            height={250}
-                            alt="Map"
-                        />
-                    </div>
-
-                    {/* amenities */}
-                    <Amenities></Amenities>
-
-                    {/* policy */}
-                    <ReturnPolicy></ReturnPolicy>
-
-                    {/* reviews & ratings */}
-                    <Reviews></Reviews>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- app/components/Reviews/index.js
+++ app/components/Reviews/index.js
@@ -1,12 +1,27 @@
 "use client";
 import React from "react";
 import styles from "./Reviews.module.scss";
+import Image from "next/image";
 
 const Reviews = () => {
     // const policy = POLICY_LIST;
     return (
-        <div className={styles["reviews-section"]}>
+        <div className={styles["reviews"]}>
             <h3>Reviews & Ratings</h3>
+
+            <div className="reviews__ratings">
+                <div className="reviews__ratings-overall">
+                    <h2>4.5</h2>
+                    <Image
+                        src="/assets/images/ratings.svg"
+                        width={20}
+                        height={20}
+                        alt="Star Rating"
+                    />
+                    <p>(Based on 563 Ratings)</p>
+                </div>
+                <div className="reviews__ratings-distribution"></div>
+            </div>
         </div>
     );
 };
