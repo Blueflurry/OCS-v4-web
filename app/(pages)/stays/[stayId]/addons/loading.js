2b6f254f42732867737b9b3898dfcfa23eae23c9
mskn06 <muskaan24mj@gmail.com>
2025-04-07 15:09:15 +0530

    updated loading

--- app/(pages)/stays/[stayId]/addons/loading.js
+++ app/(pages)/stays/[stayId]/addons/loading.js
@@ -15,7 +15,7 @@ export default function Loading() {
 
     return (
         <div className={styles["loaderContainer"]}>
-            <svg width="794" height="793" viewBox="0 0 794 793" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles["svg"]}>
+            {/* <svg width="794" height="793" viewBox="0 0 794 793" fill="none" xmlns="http://www.w3.org/2000/svg" className={styles["svg"]}>
                 <path
                     d="M27 440.332C26.9997 -205.951 935.341 -33.9157 738.716 540.119H60.2364C80.8898 596.448 99.1364 619.952 134.732 658.165H639.007C545.815 779.13 388.223 783.521 257.362 744.121M134.732 440.332C120.704 21.743 689.938 48.3073 656.184 440.332M250.996 436.857C232.021 171.009 578.664 203.17 540.673 436.857"
                     stroke="#FFB401"
@@ -24,7 +24,14 @@ export default function Loading() {
                     strokeLinejoin="round"
                     className={styles["svg-elem-1"]}
                 />
-            </svg>
+            </svg> */}
+            <video
+                src="/assets/animations/logo.mp4"
+                loop
+                autoPlay
+                muted
+                className={styles["svg"]}
+            ></video>
             <span>Finding best stay options for you...</span>
         </div>
     );
